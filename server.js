9acd94c231d61383772933b176092faf30b0e2e6
OverAcheiever <keshava.reddy.100580@standrewsindia.com>
2021-05-13 22:29:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,41 +1,40 @@
 const express = require('express'),
-      bodyParser = require("body-parser"),
-      port = process.env.PORT || 80;
-      app = express(),
-      WebSocket = require('ws'),
-      wss = new WebSocket.Server({ noServer: true, path: '/rally/alerts/websocket'}),
-      server = app.listen(port, () => console.log(`Server is live at ${port}`));
-      app.use(bodyParser.json());
+    app = express(),
+    bodyParser = require("body-parser"),
+    port = process.env.PORT || 80,
+    WebSocket = require('ws'),
+    wss = new WebSocket.Server({ noServer: true, path: '/websocket' }),
+    server = app.listen(port, () => console.log(`Server is live at ${port}`));
+app.use(bodyParser.json());
 
-var current_users = 0
-    total_users = 0
-//WS
-wss.on('connection', (ws, req) =>{
-    total_users ++;
-    current_users ++;
+var current_users = 0,
+    total_users = 0;
+
+wss.on('connection', (ws, req) => {
+    total_users++;
+    current_users++;
     console.log(`Online: ${current_users}, Total: ${total_users}`);
     ws.send("Connection Sucessful");
 });
 
-wss.on('close', function () {
-    current_users --;
-    console.log(`Online: ${current_users}`)
+wss.on('close', function() {
+    current_users--;
+    console.log(`Online: ${current_users}`);
 });
-// SERVER
+
 app.post("/POST/rally/creatorcoin/donate/OARPGGRRBPMKSIES", (req, res) => {
-    let payload = req.body
+    let payload = req.body;
     console.log(payload);
-    res.status(200);
-    res.send(payload);
+    res.sendStatus(200).send(payload)
     echo(payload);
 });
 server.on('upgrade', (request, socket, head) => {
-  wss.handleUpgrade(request, socket, head, socket => {
-    wss.emit('connection', socket, request);
-  });
+    wss.handleUpgrade(request, socket, head, socket => {
+        wss.emit('connection', socket, request);
+    });
 });
-// Send Webhook to Websocket Clients
-function echo(webhook){
+
+function echo(webhook) {
     wss.clients.forEach(function each(client) {
         client.send(JSON.stringify(webhook));
     });
