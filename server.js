4cbb03e73cf703c56375a6eb99c3198b8b21e7f2
OverAcheiever <keshava.reddy.100580@standrewsindia.com>
2021-06-21 18:54:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,37 +1,46 @@
 const express = require('express')
-const app = express()
+const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
-    path: "/",
+    path: "/websocket",
     cors: {
         origins: "*:*",
         methods: ["GET", "POST"]
     },
-    transports: ['websocket', 'polling']
 });
+const PORT = process.env.PORT || 3000;
 const bodyParser = require("body-parser")
 app.use(bodyParser.json());
-const websocket_port = 3000 || process.env.PORT;
-const webhook_port = 9000 || process.env.PORT;
 
-app.get('/', (res) => {
+app.get('/', function(req, res) {
     res.sendStatus(401)
-});
+})
+
+app.get('/api/code/alerts', (req, res) => {
+    console.log(__dirname + "/Alerts/client.html")
+    res.sendFile(__dirname + "/Alerts/client.html")
+})
 
 app.post('/POST/rally/creatorcoin/donate/OARPGGRRBPMKSIES', (req, res) => {
+    io.emit("donate", req.body)
     console.log(req.body);
     res.sendStatus(200);
-    io.emit("donate", req.body)
 })
 
 app.post('/POST/rally/creatorcoin/purchase/7ZLUQH7BDQKVSAGM', (req, res) => {
     console.log(req.body);
     res.sendStatus(200);
-    io.emit("purchase", req.body)
+    emit(req.body);
 })
 
 io.on('connection', (socket) => {
     console.log('a user connected');
 });
 
-server.listen(process.env.PORT)
+server.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+});
+
+function emit(body) {
+    io.emit("donate", body)
+}
