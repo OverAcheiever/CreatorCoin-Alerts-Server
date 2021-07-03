05662cafa78641d22084a4368bca88cc6d184a93
OverAcheiever <75264797+OverAcheiever@users.noreply.github.com>
2021-07-03 19:47:06 +0530

    v2

--- server.js
+++ server.js
@@ -1,46 +1,70 @@
-const express = require('express')
+var fs = require("fs");
+const express = require("express");
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server, {
-    path: "/websocket",
-    cors: {
-        origins: "*:*",
-        methods: ["GET", "POST"]
-    },
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+  path: "/websocket",
+  cors: {
+    origins: "*:*",
+    methods: ["GET", "POST"],
+  },
 });
+
 const PORT = process.env.PORT || 3000;
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
+
 app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
-    res.sendStatus(401)
-})
+app.get("/", function (req, res) {
+  res.sendStatus(401);
+});
 
-app.get('/api/code/alerts', (req, res) => {
-    console.log(__dirname + "/Alerts/client.html")
-    res.sendFile(__dirname + "/Alerts/client.html")
-})
+app.get("api/code/alerts", (req, res) => {
+  fs.readFile("alerts.txt", "utf8", function (err, data) {
+    res.send({
+      verion: 2.0,
+      code: data,
+    });
+  });
+});
 
-app.post('/POST/rally/creatorcoin/donate/OARPGGRRBPMKSIES', (req, res) => {
-    res.sendStatus(200);
-    console.log(req.body);
-    emit(req.body, "donation");
-})
+app.post("/POST/rally/creatorcoin/donate/OARPGGRRBPMKSIES", (req, res) => {
+   res.sendStatus(200);
+   payload = req.body.data;
+   if (payload.showUsername == false) {
+     payload.fromUsername = "null";
+   }
+   if (payload.showMemo == false) {
+     payload.memo = "";
+   }
+   req.body.data = payload;
+   emit(req.body, "donation");
+   console.log(req.body);
+});
 
-app.post('/POST/rally/creatorcoin/purchase/7ZLUQH7BDQKVSAGM', (req, res) => {
-    console.log(req.body);
-    res.sendStatus(200);
-    emit(req.body, "purchase");
-})
+app.post("/POST/rally/creatorcoin/purchase/7ZLUQH7BDQKVSAGM", (req, res) => {
+  res.sendStatus(200);
+  payload = req.body.data
+  console.log(payload.showUsername)
+  if (payload.showUsername == false) {
+    payload.fromUsername = "null";
+  }
+  if (payload.showMemo == false) {
+    payload.memo = "";
+  }
+  req.body.data = payload;
+  emit(req.body, "purchase");
+  console.log(req.body);
+});
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
 });
 
 server.listen(PORT, () => {
-    console.log(`Server running on port: ${PORT}`);
+  console.log(`Server running on port: ${PORT}`);
 });
 
 function emit(body, eventType) {
-    io.emit(eventType, body)
+  io.emit(eventType, body);
 }
